88b44f5f9ffb5c9219fc191da5b957a5aebe4fcb
JacquysBarbosadaSilva <jacquys0801@gmail.com>
2025-03-18 11:38:57 -0300

    nav e footer professores

--- sesicomunica/professores/inicialprof.php
+++ sesicomunica/professores/inicialprof.php
@@ -1,13 +1,19 @@
+<?php 
+    include 'navprof.php'; 
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../css/cssprof/inicialprof.css">
-    <link rel="shortcut icon" type="" href="../img/icon.png">
-    <title>Página inicial</title>
-</head>
-<body>
-    
-</body>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="../css/cssprof/inicialprof.css">
+        <link rel="shortcut icon" type="" href="../img/icon.png">
+        <title>Página inicial</title>
+    </head>
+    <body>
+        <?php include 'footerprof.php'?>
+    </body>
+    <script src="../js/main.js"></script>
 </html>
