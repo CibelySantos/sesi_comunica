b4c2db31dd87ccbf6877704326389b8e3a89678d
JacquysBarbosadaSilva <jacquys0801@gmail.com>
2025-03-18 10:46:15 -0300

    troca de arquivos para PHP

---
+++ sesicomunica/aluno/pesquisaaluno.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/cssaluno/pesquisaaluno.css">
+    <link rel="shortcut icon" type="" href="../img/icon.png">
+    <title>Pesquise</title>
+</head>
+<body>
+    
+</body>
+</html>
---
+++ sesicomunica/professores/notificacoesprof.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/cssprof/notificacoesprof.css">
+    <link rel="shortcut icon" type="" href="../img/icon.png">
+    <title>Notificações</title>
+</head>
+<body>
+    
+</body>
+</html>
