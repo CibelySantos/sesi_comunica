d9f28db4c9292f2712296346f2ff87fad35559a5
JacquysBarbosadaSilva <jacquys0801@gmail.com>
2025-03-18 10:54:40 -0300

    troca de arquivos para php

---
+++ sesicomunica/adm/nav.php
@@ -0,0 +1,28 @@
+<style>@import url('https://fonts.googleapis.com/css2?family=Gabarito:wght@400..900&display=swap');
+        </style>
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?              family=Gabarito:wght@400900display=swap" rel="stylesheet">
+
+<div class="alinhamento-nav">
+    <div>
+        <img class="logo-comunica" src="../img/PROJETO GG.png" alt="">
+    </div>
+
+    <div class="perfil-user">
+        <img class="icone-user" src="../img/icone-usuario.png" alt="">
+    </div>
+
+    
+</div>
+
+<div class="menu">
+    <div class="hamburger" onclick="openMenu()">☰ Menu</div>
+    <div class="overlay" id="overlay" onclick="closeMenu()"></div>
+    <div class="menu-container" id="menu">
+        <a href="#">Página Inicial</a>
+        <a href="#">Administrativo</a>
+        <a href="#">Calendário</a>
+        <span class="close-btn" onclick="closeMenu()">✖</span>
+    </div>
+</div>
