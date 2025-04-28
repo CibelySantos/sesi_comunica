a98a52668c1f3df8c192b6f7410e620351a3b477
gabrielMoreira2007 <gabrielmgseg@gmail.com>
2025-04-27 22:36:18 -0300

    alterações cardápio aluno 27/04/2025

--- sesicomunica/aluno/cardapioaluno.php
+++ sesicomunica/aluno/cardapioaluno.php
@@ -13,8 +13,8 @@
         <title>Cardápio</title>
     </head>
     <body>
-        <h1 style="display: flex; align-items: center; justify-content: center; margin-top: 20px; font-family: Arial, sans-serif;">Cardápio Atual:</h1>
-        <img src="../img/cardapio.png" alt="Cardápio" style="display: block; margin: 30px auto 10px auto; width: 50%; max-width: 800px;">
+        <h1 style="display: flex; align-items: center; justify-content: center; margin-top: 80px; font-family: Arial, sans-serif;">Cardápio Atual:</h1>
+        <img src="../img/cardapio.png" alt="Cardápio" style="display: block; margin: 30px auto 10px auto; width: 50%; max-width: 800px; box-shadow: 10px 10px 20px rgba(1, 1, 1, 0.5); border-radius: 10px;">
         <?php include 'footeraluno.php'?>
     </body>
     <script src="../js/nav-aluno.js"></script>
