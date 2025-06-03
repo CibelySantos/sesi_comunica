3d4e7f4f2794b7739f2696f9e3825ef5de264ce8
gabrielMoreira2007 <gabrielmgseg@gmail.com>
2025-06-03 16:11:55 -0300

    navbar

--- index.php
+++ index.php
@@ -9,22 +9,6 @@
         <link rel="stylesheet" href="sesicomunica/css/style.css">
         <link rel="shortcut icon" type="" href="../sesicomunica/img/icon.png">
   <title>Página inicial - SESI Comunica</title>
-  <style>
-    .menu-container {
-    display: flex;
-    gap: 30px;
-    list-style: none;
-    margin: -50;
-    padding: 0 20px;
-    position: static !important;
-    margin-top: 1px;
-    transition: left 0.3s ease-in-out;
-    margin-left: 0 !important;
-    }
-    .logo-comunica {
-    max-height: 100px;
-    } 
-  </style>
 </head>
 
 <body>
